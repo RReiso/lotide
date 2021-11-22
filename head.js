9f51bd2418ad7ec434e02835943d81de0f64576c
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-22 23:04:29 +0000

    Write function head

---
+++ head.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}] ✅`);
+  else console.log(`❌ Assertion Failed: [${actual}] !== [${expected}] ❌`);
+};
+
+const head = (arr) => {
+  return arr[0] || undefined;
+};
+
+// TEST CODE
+console.log(head([1, 2, 3]));
+console.log(head([2]));
+console.log(head([]));
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hellllo");
