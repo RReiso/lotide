5c692acc6ab4a287eccf172b0636d01175cac503
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-23 23:36:16 +0000

    Remove elements from array

---
+++ without.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(actual, expected) {
+  const isEqual = eqArrays(actual,expected);
+  if (isEqual)
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}] ✅`);
+  else console.log(`❌ Assertion Failed: [${actual}] !== [${expected}] ❌`);
+};
+
+const eqArrays = (arr1, arr2)=>{
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+
+const without = (source, itemsToRemove)=>{
+  let newArr = [];
+  for (let el of source) {
+    let match = false;
+    for (let item of itemsToRemove) {
+      if (el === item) match = true;
+    }
+    if (match === false) newArr.push(el);
+  }
+  return newArr;
+};
+
+
+assertArraysEqual(without([1, 2, 3], [1]),[2,3]); // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]),["1","2"]); // => ["1", "2"]
+assertArraysEqual(without(["1", "2", "3"], []),["1","2","3"]); // => ["1", "2"]
+assertArraysEqual(without([], [1,2]),[]); // => ["1", "2"]
