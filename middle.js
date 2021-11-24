30b69724e06a1c3687a278cdb1d1fe0648b50fe4
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-24 16:51:14 +0000

    Refactor

--- middle.js
+++ middle.js
@@ -16,7 +16,7 @@ const eqArrays = (arr1, arr2)=>{
 const middle = (arr) => {
   if (arr.length < 3) return [];
   if (arr.length % 2 === 1) return [arr[Math.floor(arr.length / 2)]];
-  if (arr.length % 2 === 0) return [arr[arr.length / 2 - 1], arr[arr.length / 2]];
+  else return [arr[arr.length / 2 - 1], arr[arr.length / 2]];
 };
 
 
