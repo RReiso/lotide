44208a6236017ad3add1d77201b3fd27fcc3ff29
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-24 22:30:05 +0000

    Refactor

--- eqObjects.js
+++ eqObjects.js
@@ -18,9 +18,13 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
 
   for (const key in object1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) return false;
-    } else if (object1[key] !== object2[key]) return false;
+    let obj1Val = object1[key];
+    let obj2Val = object2[key];
+    let bothArrays = Array.isArray(obj1Val) &&
+      Array.isArray(obj2Val);
+
+    if (bothArrays && !eqArrays(obj1Val, obj2Val)) return false;
+    if (obj1Val !== obj2Val) return false;
   }
   return true;
 };
@@ -38,4 +42,4 @@ const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2),false); // => false
+assertEqual(eqObjects(cd, cd2), false); // => false
