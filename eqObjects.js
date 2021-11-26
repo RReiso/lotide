a104013a8a58f05c33f75e4d11653cbd13b6f38a
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-26 22:31:12 +0000

    Add coments

--- eqObjects.js
+++ eqObjects.js
@@ -20,13 +20,16 @@ const eqObjects = function(object1, object2) {
   for (const key in object1) {
     let obj1Val = object1[key];
     let obj2Val = object2[key];
-    let isBothArrays = Array.isArray(obj1Val) && Array.isArray(obj2Val);
 
+    // check if both values are arrays
+    let isBothArrays = Array.isArray(obj1Val) && Array.isArray(obj2Val);
     if (isBothArrays && !eqArrays(obj1Val, obj2Val)) return false;
+
+    //check if both values are objects
     let isBothObjects = (typeof obj1Val === "object" && typeof obj2Val === "object");
-    if (isBothObjects) {
-      return eqObjects(obj1Val, obj2Val);
-    }
+    if (isBothObjects) return eqObjects(obj1Val, obj2Val);
+
+    // check if both values are not the same primitive values
     if (obj1Val !== obj2Val) return false;
   }
   return true;
@@ -48,8 +51,8 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
 
 
-console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
-console.log(eqObjects({ a: { y: {m: 3}, z: 1 }, b: 2 }, { a: { y: {m: 3}, z: 1 }, b: 2 })); //true/
-console.log(eqObjects({ a: { y: {m: ["2", 3, 4]}, z: 1 }, b: 2 }, { a: { y: {m: ["2", 3, 4]}, z: 1 }, b: 2 })); //true/
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }),false); // => false
+assertEqual(eqObjects({ a: { y: {m: 3}, z: 1 }, b: 2 }, { a: { y: {m: 3}, z: 1 }, b: 2 }),true); //true/
+assertEqual(eqObjects({ a: { y: {m: ["2", 3, 4]}, z: 1 }, b: 2 }, { a: { y: {m: ["2", 3, 4]}, z: 1 }, b: 2 }),true); //true/
