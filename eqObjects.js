079645dff79e4d38c99f1824a182982be535e95a
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-26 21:34:45 +0000

    Compare nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -20,9 +20,13 @@ const eqObjects = function(object1, object2) {
   for (const key in object1) {
     let obj1Val = object1[key];
     let obj2Val = object2[key];
-    let bothArrays = Array.isArray(obj1Val) && Array.isArray(obj2Val);
+    let isBothArrays = Array.isArray(obj1Val) && Array.isArray(obj2Val);
 
-    if (bothArrays && !eqArrays(obj1Val, obj2Val)) return false;
+    if (isBothArrays && !eqArrays(obj1Val, obj2Val)) return false;
+    let isBothObjects = (typeof obj1Val === "object" && typeof obj2Val === "object");
+    if (isBothObjects) {
+      return eqObjects(obj1Val, obj2Val);
+    }
     if (obj1Val !== obj2Val) return false;
   }
   return true;
@@ -42,3 +46,10 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+console.log(eqObjects({ a: { y: {m: 3}, z: 1 }, b: 2 }, { a: { y: {m: 3}, z: 1 }, b: 2 })); //true/
+console.log(eqObjects({ a: { y: {m: ["2", 3, 4]}, z: 1 }, b: 2 }, { a: { y: {m: ["2", 3, 4]}, z: 1 }, b: 2 })); //true/
